0046a648f2c59f9add506cdcdbc6e212e6b69256
DJ Nastor <lukulurecordings@gmail.com>
2026-07-31 18:51:57 +0000

    fix: secure and prepare Lukulu website deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Lukulu-WebService/',
-  plugins: [react()],
-})
+export default defineConfig({ base: process.env.GITHUB_ACTIONS ? '/Lukulu-WebService/' : '/', plugins: [react()] })
